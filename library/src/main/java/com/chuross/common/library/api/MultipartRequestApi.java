7d200fe80122be6638468f51a1fa671e3e4f30b0
chuross <azarasi.mo.mo@gmail.com>
2014-07-25 15:08:33 -0700

    ファイルアップロード用のAPIクラスを追加

---
+++ library/src/main/java/com/chuross/common/library/api/MultipartRequestApi.java
@@ -0,0 +1,28 @@
+package com.chuross.common.library.api;
+
+import com.chuross.common.library.http.HttpResponse;
+import com.chuross.common.library.util.FutureUtils;
+import com.chuross.common.library.util.HttpClientUtils;
+import com.google.common.util.concurrent.MoreExecutors;
+import org.apache.http.client.config.RequestConfig;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+public abstract class MultipartRequestApi<T extends Result<?>> extends PostRequestApi<T> {
+
+    protected abstract String getUploadParameterName();
+
+    protected abstract byte[] getData();
+
+    @Override
+    protected Callable<HttpResponse> getHttpResponseCallable(RequestConfig config, int retryCount) {
+        final Future<HttpResponse> future = HttpClientUtils.post(MoreExecutors.sameThreadExecutor(), getUrl(), getParameter(), getUploadParameterName(), getData(), getRequestHeaders(), config, retryCount);
+        return new Callable<HttpResponse>() {
+            @Override
+            public HttpResponse call() throws Exception {
+                return FutureUtils.getOrNull(future);
+            }
+        };
+    }
+}
