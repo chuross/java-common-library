1d1757914e190d7049d4b95e591ddd326ad08575
chuross <azarasi.mo.mo@gmail.com>
2014-07-25 15:00:02 -0700

    XMLUtilsを追加

---
+++ library/src/main/java/com/chuross/common/library/util/XmlUtils.java
@@ -0,0 +1,87 @@
+package com.chuross.common.library.util;
+
+import org.simpleframework.xml.core.Persister;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.concurrent.Callable;
+
+public final class XmlUtils {
+
+    private XmlUtils() {
+    }
+
+    public static <T> T read(final Class<T> clazz, final File file) {
+        return ExecuteUtils.callOrNull(new Callable<T>() {
+            @Override
+            public T call() throws Exception {
+                return new Persister().read(clazz, file);
+            }
+        });
+    }
+
+    public static <T> T read(final Class<T> clazz, final File file, final boolean strict) {
+        return ExecuteUtils.callOrNull(new Callable<T>() {
+            @Override
+            public T call() throws Exception {
+                return new Persister().read(clazz, file, strict);
+            }
+        });
+    }
+
+    public static <T> T read(final Class<T> clazz, final InputStream inputStream) {
+        return ExecuteUtils.callOrNull(new Callable<T>() {
+            @Override
+            public T call() throws Exception {
+                return new Persister().read(clazz, inputStream);
+            }
+        });
+    }
+
+    public static <T> T read(final Class<T> clazz, final InputStream inputStream, final boolean strict) {
+        return ExecuteUtils.callOrNull(new Callable<T>() {
+            @Override
+            public T call() throws Exception {
+                return new Persister().read(clazz, inputStream, strict);
+            }
+        });
+    }
+
+    public static <T> T read(final Class<T> clazz, final Reader reader) {
+        return ExecuteUtils.callOrNull(new Callable<T>() {
+            @Override
+            public T call() throws Exception {
+                return new Persister().read(clazz, reader);
+            }
+        });
+    }
+
+    public static <T> T read(final Class<T> clazz, final Reader reader, final boolean strict) {
+        return ExecuteUtils.callOrNull(new Callable<T>() {
+            @Override
+            public T call() throws Exception {
+                return new Persister().read(clazz, reader, strict);
+            }
+        });
+    }
+
+    public static <T> T read(final Class<T> clazz, final String source) {
+        return ExecuteUtils.callOrNull(new Callable<T>() {
+            @Override
+            public T call() throws Exception {
+                return new Persister().read(clazz, source);
+            }
+        });
+    }
+
+    public static <T> T read(final Class<T> clazz, final String source, final boolean strict) {
+        return ExecuteUtils.callOrNull(new Callable<T>() {
+            @Override
+            public T call() throws Exception {
+                return new Persister().read(clazz, source, strict);
+            }
+        });
+    }
+
+}
