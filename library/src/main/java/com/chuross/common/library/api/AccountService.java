6115340b8406db2d20e4ea789a62161edf4172fc
chuross <azarasi.mo.mo@gmail.com>
2014-08-05 01:40:22 +0900

    名前変更

--- library/src/main/java/com/chuross/common/library/api/AccountService.java
+++ library/src/main/java/com/chuross/common/library/api/AccountService.java
@@ -9,7 +9,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Future;
 
-public abstract class AcoountService<SESSION> {
+public abstract class AccountService<SESSION> {
 
     private OnLoginSessionChangedListener<SESSION> listener;
 
