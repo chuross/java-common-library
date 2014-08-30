35bfb717b846fdf9a1a63aa5f615327a4ebf2a60
chuross <azarasi.mo.mo@gmail.com>
2014-08-31 02:06:58 +0900

    executeWithAuthenticationでアカウント情報を取るようにした

--- library/src/main/java/com/chuross/common/library/api/AccountService.java
+++ library/src/main/java/com/chuross/common/library/api/AccountService.java
@@ -7,33 +7,30 @@ import com.chuross.common.library.util.MethodCallUtils;
 import com.google.common.util.concurrent.MoreExecutors;
 import org.apache.http.client.config.RequestConfig;
 
-import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Future;
 
-public abstract class AccountService<SESSION, AUTH_RESULT extends AuthenticationResult<SESSION, ?>> {
+public abstract class AccountService<ACCOUNT, SESSION, AUTH_RESULT extends AuthenticationResult<SESSION, ?>> {
 
-    private OnLoginSessionChangedListener<SESSION> listener;
+    private OnLoginSessionChangedListener<ACCOUNT, SESSION> listener;
 
-    protected abstract void onLoginSessionChanged(SESSION session);
+    protected abstract Callable<Api<AUTH_RESULT>> getAuthenticationApiCallable(ACCOUNT account);
 
-    protected abstract Callable<Api<AUTH_RESULT>> getAuthenticationApiCallable();
-
-    public void setOnLoginSessionChangedListener(OnLoginSessionChangedListener<SESSION> listener) {
+    public void setOnLoginSessionChangedListener(OnLoginSessionChangedListener<ACCOUNT, SESSION> listener) {
         this.listener = listener;
     }
 
-    protected <RESULT extends AuthenticatedResult<?>> Future<RESULT> executeWithAuthentication(Executor executor, final RequestConfig config, final int retryCount, final Callable<Api<RESULT>> apiCallable) {
+    protected <RESULT extends AuthenticatedResult<?>> Future<RESULT> executeWithAuthentication(Executor executor, final ACCOUNT account, final RequestConfig config, final int retryCount, final Callable<Api<RESULT>> apiCallable) {
         return FutureUtils.executeOrNull(executor, new Callable<RESULT>() {
             @Override
             public RESULT call() throws Exception {
-                return executeWithAuthentication(config, retryCount, apiCallable);
+                return executeWithAuthentication(account, config, retryCount, apiCallable);
             }
         });
     }
 
-    private <RESULT extends AuthenticatedResult<?>> RESULT executeWithAuthentication(RequestConfig config, int retryCount, final Callable<Api<RESULT>> apiCallable) {
+    private <RESULT extends AuthenticatedResult<?>> RESULT executeWithAuthentication(ACCOUNT account, RequestConfig config, int retryCount, final Callable<Api<RESULT>> apiCallable) {
         Api<RESULT> api = MethodCallUtils.callOrNull(apiCallable);
         if(api == null) {
             return null;
@@ -45,13 +42,13 @@ public abstract class AccountService<SESSION, AUTH_RESULT extends Authentication
         if(!result.isExpiredLoginSession()) {
             return result;
         }
-        Api<AUTH_RESULT> authApi = MethodCallUtils.callOrNull(getAuthenticationApiCallable());
+        Api<AUTH_RESULT> authApi = MethodCallUtils.callOrNull(getAuthenticationApiCallable(account));
         if(authApi == null) {
             return null;
         }
         AUTH_RESULT authResult = FutureUtils.getOrNull(authApi.execute(MoreExecutors.sameThreadExecutor(), config, retryCount));
         if(!authResult.isInvalidAccountInfo() && listener != null) {
-            listener.onAccountChanged();
+            listener.onAuthenticationFailed(account);
         }
         if(!authResult.isSuccess()) {
             return result;
@@ -60,13 +57,13 @@ public abstract class AccountService<SESSION, AUTH_RESULT extends Authentication
         if(listener != null) {
             listener.onSessionChanged(newSession);
         }
-        onLoginSessionChanged(newSession);
         return FutureUtils.getOrNull(MethodCallUtils.callOrNull(apiCallable).execute(MoreExecutors.sameThreadExecutor(), config, retryCount));
     }
 
-    public interface OnLoginSessionChangedListener<SESSION> {
-        public void onAccountChanged();
+    public interface OnLoginSessionChangedListener<ACCOUNT, SESSION> {
+
+        public void onAuthenticationFailed(ACCOUNT account);
+
         public void onSessionChanged(SESSION session);
     }
-
 }
