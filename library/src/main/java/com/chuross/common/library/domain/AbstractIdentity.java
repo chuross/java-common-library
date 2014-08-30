d4b95354070e3b99eece053e5b9536320e14f5e1
chuross <azarasi.mo.mo@gmail.com>
2014-08-30 16:09:47 +0900

    AbstractIdentity追加

---
+++ library/src/main/java/com/chuross/common/library/domain/AbstractIdentity.java
@@ -0,0 +1,5 @@
+package com.chuross.common.library.domain;
+
+public abstract class AbstractIdentity<T> extends AbstractValueObject implements Identity<T> {
+
+}
--- library/src/main/java/com/chuross/common/library/domain/Identity.java
+++ library/src/main/java/com/chuross/common/library/domain/Identity.java
@@ -2,7 +2,7 @@ package com.chuross.common.library.domain;
 
 import java.io.Serializable;
 
-public interface Identity<T> extends Serializable {
+public interface Identity<T> extends Serializable, ValueObject {
 
     public T getIdentity();
 }
