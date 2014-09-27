0216715fefe4919872e3133c7768a559d5f4fdac
chuross <azarasi.mo.mo@gmail.com>
2014-09-28 00:20:21 +0900

    リフレクションまわりの処理を追加した

--- library/src/main/java/com/chuross/common/library/util/RefrectionUtils.java
+++ library/src/main/java/com/chuross/common/library/util/RefrectionUtils.java
@@ -4,6 +4,7 @@ import org.apache.commons.beanutils.PropertyUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
 import java.util.concurrent.Callable;
 
 public class RefrectionUtils {
@@ -17,7 +18,18 @@ public class RefrectionUtils {
         return MethodCallUtils.callOrNull(new Callable<T>() {
             @Override
             public T call() throws Exception {
-                return (T) PropertyUtils.getSimpleProperty(target, key);
+                return (T) PropertyUtils.getProperty(target, key);
+            }
+        });
+    }
+
+    public static <T> T getProperty(final Class<?> clazz, final Object target, final String key, final boolean accessible) {
+        return MethodCallUtils.callOrNull(new Callable<T>() {
+            @Override
+            public T call() throws Exception {
+                Field field = clazz.getDeclaredField(key);
+                field.setAccessible(accessible);
+                return (T) field.get(target);
             }
         });
     }
@@ -27,6 +39,17 @@ public class RefrectionUtils {
             @Override
             public void run() throws Exception {
                 PropertyUtils.setProperty(target, key, value);
+            }
+        });
+    }
+
+    public static void setProperty(final Class<?> clazz, final Object target, final String key, final Object value, final boolean accessible) {
+        MethodCallUtils.callQuietly(new Runner() {
+            @Override
+            public void run() throws Exception {
+                Field field = clazz.getDeclaredField(key);
+                field.setAccessible(accessible);
+                field.set(target, value);
             }
         });
     }
