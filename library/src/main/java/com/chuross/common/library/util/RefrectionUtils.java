5006995351480e40741f792cdafe14277538d37e
chuross <azarasi.mo.mo@gmail.com>
2014-09-27 21:37:32 +0900

    リフレクションまわりの処理修正

--- library/src/main/java/com/chuross/common/library/util/RefrectionUtils.java
+++ library/src/main/java/com/chuross/common/library/util/RefrectionUtils.java
@@ -4,6 +4,8 @@ import org.apache.commons.beanutils.PropertyUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.Callable;
+
 public class RefrectionUtils {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RefrectionUtils.class);
@@ -11,12 +13,21 @@ public class RefrectionUtils {
     private RefrectionUtils() {
     }
 
-    public static <T> T getProperty(Object object, String key) {
-        try {
-            return (T) PropertyUtils.getSimpleProperty(object, key);
-        } catch(Exception e) {
-            LOGGER.error("getProperty error", e);
-            return null;
-        }
+    public static <T> T getProperty(final Object target, final String key) {
+        return MethodCallUtils.callOrNull(new Callable<T>() {
+            @Override
+            public T call() throws Exception {
+                return (T) PropertyUtils.getSimpleProperty(target, key);
+            }
+        });
+    }
+
+    public static void setProperty(final Object target, final String key, final Object value) {
+        MethodCallUtils.callQuietly(new Runner() {
+            @Override
+            public void run() throws Exception {
+                PropertyUtils.setProperty(target, key, value);
+            }
+        });
     }
 }
